67ab249363ff282780684b9a350ddd57c1baaf08
a-dugonin <a_dugonin@mail.ru>
2024-02-19 21:04:18 +0300

    Реализован функционал в рамках задачи с использованиемлогирования

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
-from task_1.number_squares import user_value, NumberSquares, number_squares, number_squares_gen_expression
+import logging
+from task_1.number_squares import get_user_data, NumberSquares, number_squares_generator
+
+logger = logging.getLogger(__name__)
+fileHandler = logging.FileHandler(filename='application_log.log', encoding='utf-8')
+logging.basicConfig(format='[%(levelname)-10s] [модуль:%(name)22s] %(asctime)-25s - %(message)s',
+                    handlers=[fileHandler], level=logging.INFO)
+
 if __name__ == '__main__':
-    number_squares_iterator = NumberSquares(user_value)
-    print(*number_squares_iterator)
+    logger.info("Скрипт запущен")
+    user_data = get_user_data()
+    num_squares = NumberSquares(user_data)
+    print(next(num_squares))
 
-    number_squares_func_generator = number_squares(user_value)
-    print(*number_squares_func_generator)
+    print(*number_squares_generator(user_data))
 
-    print(*number_squares_gen_expression)
+    try:
+        logger.info("Создание генераторного выражения")
+        generator_expression = (num ** 2 for num in range(1, user_data + 1))
+        logger.info(f"Успешно создано генераторное выражение - generator_expression")
+        print(*generator_expression)
+    except ValueError as err:
+        logger.exception(f"Не удалось создать генераторное выражение из за следующей ошибки: {err}")
--- task_1/number_squares.py
+++ task_1/number_squares.py
@@ -1,6 +1,18 @@
+import logging
 from collections.abc import Iterator
 
-user_value = int(input("Введите значение: "))
+logger = logging.getLogger(__name__)
+
+
+def get_user_data() -> int:
+    """ Функция для получения пользовательских данных """
+    logger.info("Получение пользовательских данных")
+    try:
+        user_input = int(input("Введите числовое значение: "))
+        logger.info(f"Получено корректное значение: {user_input} ")
+        return user_input
+    except ValueError:
+        logger.error("Введено значение отличное от числового")
 
 
 class NumberSquares:
@@ -11,25 +23,34 @@ class NumberSquares:
     """
 
     def __init__(self, number: int) -> None:
-        self.number = number
+        try:
+            logger.info("Инициализация экземпляра класса NumberSquares для создания итератора")
+            self.number = number
+            logger.info("Экземпляр класса NumberSquares успешно инициализирован")
+        except ValueError:
+            logger.error("Ошибка инициализации объекта класса NumberSquares. Переданный аргумент имеет "
+                         "не корректное значение")
         self.start = 0
 
     def __iter__(self) -> Iterator[int]:
         return self
 
     def __next__(self) -> int:
+        logger.info("Запуск метода next итератора NumberSquares")
         if self.start >= self.number:
+            logger.info(f"Итератор NumberSquares пустой, возбуждено исключение StopIteration")
             raise StopIteration
         self.start += 1
+        logger.info(f"Результат выполнения метода next - {self.start ** 2}")
         return self.start ** 2
 
 
-def number_squares(number: int) -> Iterator[int]:
+def number_squares_generator(number: int) -> Iterator[int]:
     """ Функция генератор для вычисления квадратов чисел от 1 до числа введенного пользователем """
-    for item in range(1, number + 1):
-        yield item ** 2
-
-
-# Генераторное выражение для вычисления квадратов чисел
-number_squares_gen_expression = (num ** 2 for num in range(1, user_value + 1))
-print(*number_squares_gen_expression)
+    try:
+        logger.info("Запуск функции number_squares_generator")
+        for item in range(1, number + 1):
+            yield item ** 2
+        logger.info("Функция number_squares_generator отработала корректно")
+    except ValueError:
+        logger.error(f"Функция number_squares_generator получила аргумент отличный от ожидаемого: {number}")
