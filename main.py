dcdc1682c547669516427c141238e37c0fda9922
a-dugonin <a_dugonin@mail.ru>
2024-02-19 21:06:10 +0300

    Добавление файлов для инициализации проекта

--- main.py
+++
@@ -1,9 +0,0 @@
-from task_1.number_squares import user_value, NumberSquares, number_squares, number_squares_gen_expression
-if __name__ == '__main__':
-    number_squares_iterator = NumberSquares(user_value)
-    print(*number_squares_iterator)
-
-    number_squares_func_generator = number_squares(user_value)
-    print(*number_squares_func_generator)
-
-    print(*number_squares_gen_expression)
