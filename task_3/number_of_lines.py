aa2e4248e82120845f59b8dc56d608586b7c2495
a-dugonin <a_dugonin@mail.ru>
2024-02-21 20:52:43 +0300

    task_3: Реализован скрипт который подсчитывает количество строк кода в файлах с расширением .py

---
+++ task_3/number_of_lines.py
@@ -0,0 +1,33 @@
+import os
+import logging
+from itertools import tee
+from typing import Iterator
+
+logger = logging.getLogger(__name__)
+
+
+def numbers_of_lines(path: str) -> Iterator[str]:
+    logger.info("Запуск функции numbers_of_lines")
+    try:
+        files, my_files = tee(os.scandir(path), 2)
+        if not any(map(lambda file_py: file_py.name.endswith(".py"), files)):
+            logger.info("В указанной директории отсутствуют файлы с расширением .py")
+        else:
+            for file in my_files:
+                if file.is_file and file.name.endswith(".py"):
+                    with open(file.path, encoding="utf-8") as python_file:
+                        count = 0
+                        for line in python_file:
+                            if line.rstrip() and not line.lstrip().startswith("#"):
+                                count += 1
+                        yield f"В файле {file.path} - {count} строк кода"
+            logger.info("Функция numbers_of_lines отработала корректно")
+    except FileNotFoundError:
+        logger.error("Указанного каталога не существует")
+
+
+def get_files_path():
+    logger.info("Запрос каталога пользователя")
+    path_name = input("Введите адрес каталога: ")
+    logger.info(f"Функция {get_files_path.__name__} отработала корректно. Получено имя каталога: {path_name}")
+    return path_name
