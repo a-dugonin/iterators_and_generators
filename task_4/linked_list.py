97a951abb2588291b8c450bec099e61599c91130
a-dugonin <a_dugonin@mail.ru>
2024-02-27 18:31:02 +0300

    В рамках задачи реализовн класс LinkedList для создания односвязного списка

---
+++ task_4/linked_list.py
@@ -0,0 +1,109 @@
+import logging
+from typing import Any, Iterator
+
+logger = logging.getLogger(__name__)
+
+
+class Node:
+    """
+    Класс для создания узла односвязного списка
+    Аргументы:
+        value - значение узла, по умолчанию None
+    """
+
+    def __init__(self, value: Any = None) -> None:
+        logger.info("Создание узла с использованием инициализации экземпляра класса Node")
+        self.value = value
+        self.next_node = None
+        logger.info(f"Узел создан со значением {self.value}")
+
+    def __str__(self) -> str:
+        return f'[{self.value}] -> {self.next_node}'
+
+
+class LinkedList:
+    """ Класс для создания односвязного списка """
+
+    def __init__(self) -> None:
+        logger.info(f"Создание односвязного списка посредством инициализации экземпляра класса LinkedList")
+        self.head = None
+        logger.info(f"Создан пустой односвязный список")
+
+    def __iter__(self) -> Iterator[Any]:
+        """ Метод для итерирования по односвязному списку на основе генератора """
+        current = self.head
+        while current is not None:
+            yield current.value
+            current = current.next_node
+
+    def append(self, value: Any) -> None:
+        """ Метод позволяет добавить новый узел в конец связного списка
+        Аргументы:
+            value - значение узла
+        """
+        logger.info(f"Добавление элемента {value} в конец списка")
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            return
+        last_node = self.head
+        while last_node.next_node:
+            last_node = last_node.next_node
+        last_node.next_node = new_node
+        logger.info(f"Элемент {value} добавлен в конец списка")
+
+    def get(self, index: int) -> Any:
+        """
+        Метод позволяет получить значение элемента узла списка по его индексу
+        Аргументы:
+            index - индекс узла, элемент которого необходимо получить
+        """
+        logger.info("Получение элемента по индексу")
+        last_node = self.head
+        node_index = 0
+        try:
+            while node_index <= index:
+                if node_index == index:
+                    element = last_node.value
+                    logger.info(f"Элемент успешно получен. Значение элемента - {element}")
+                    return element
+                node_index += 1
+                last_node = last_node.next_node
+        except TypeError:
+            logger.error(f"Передаваемый индекс должен быть числовой. В функцию передано значение {index}")
+
+    def remove(self, index: int) -> None:
+        """
+        Метод позволяет удалить узел из списка по его индексу
+        Аргументы:
+            index - индекс узла, который необходимо удалить из списка
+        """
+        logger.info("Удаление узла по индексу")
+        current_node = self.head
+        prev_node = current_node
+        node_index = 0
+        try:
+
+            if index == 0:
+                self.head = current_node.next_node
+                return
+
+            while node_index <= index:
+                if node_index == index:
+                    break
+                prev_node = current_node
+                current_node = current_node.next_node
+                node_index += 1
+            prev_node.next_node = current_node.next_node
+            logger.info("Узел успешно удален")
+        except TypeError:
+            logger.error(f"Передаваемый индекс должен быть числовой. В функцию передано значение {index}")
+
+    def __str__(self) -> str:
+        linked_list_str = '['
+        current_node = self.head
+        while current_node.next_node:
+            linked_list_str += str(current_node.value) + ', '
+            current_node = current_node.next_node
+        linked_list_str += str(current_node.value) + ']'
+        return f'{linked_list_str}'
